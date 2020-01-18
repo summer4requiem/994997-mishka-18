97d53fb1c3129c76ff50881f15cc01987fa8c318
summer4requiem <summer.requiem96@gmail.com>
2020-01-18 14:48:03 +0300

    Правки перед второй защитой

--- source/js/modal.js
+++ source/js/modal.js
@@ -6,10 +6,10 @@ var modalCart = document.querySelector(".modal-cart");
 thingsBtns.forEach (function (el) {
     el.addEventListener ("click", function(evt){
     evt.preventDefault();
-    modalShow.classList.remove("overlay_closed");
-    modalCart.classList.remove("overlay_closed");
-    modalShow.classList.add("modal-overlay--opened");
-    modalCart.classList.add("modal-overlay--opened");
+    modalShow.classList.remove("modal-overlay__closed");
+    modalCart.classList.remove("modal-overlay__closed");
+    modalShow.classList.add("modal-overlay__opened");
+    modalCart.classList.add("modal-overlay__opened");
   })
 })
 
@@ -17,8 +17,8 @@ thingsBtns.forEach (function (el) {
 modalShow.addEventListener ("click", function(evt){
   evt.preventDefault();
   if (evt.target === modalShow) {
-    modalShow.classList.remove("modal-overlay--opened");
-    modalShow.classList.add("overlay_closed");
-    modalCart.classList.add("overlay_closed");
+    modalShow.classList.remove("modal-overlay__opened");
+    modalShow.classList.add("modal-overlay__closed");
+    modalCart.classList.add("modal-overlay__closed");
   }
 })
